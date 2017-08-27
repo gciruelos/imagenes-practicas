5b54ac5d05bfa6d2d1857602485d274949903977
Gonzalo Ciruelos <gonzalo.ciruelos@gmail.com>
2017-08-27 05:39:16 -0300

    mas

---
+++ practica2/ej01-b.py
@@ -0,0 +1,35 @@
+
+import numpy as np
+from PIL import Image
+from sys import argv
+import side_by_side
+
+L = 256
+
+def histogram(im):
+	return side_by_side.histogram_rgb(im)
+
+
+def uniform_hist(im):
+    dom, histogram_r, accum_r, histogram_g, accum_g, histogram_b, accum_b  = histogram(im)
+    def w_dot(r):
+        wr = accum_r[r[0]]
+        wg = accum_g[r[1]]
+        wb = accum_b[r[2]]
+        return list(map(lambda w: int(w * L - 0.5), [wr, wg, wb]))
+        # # Es mas lento hacer:
+        # for n in range(L):
+        #     if n+1 >= int(w * L + 0.4999):
+        #         if n != n_: print (w, L, w*L, n, n_)
+        #         return n
+        # print(L, n, w, r)
+    ret = im.copy()
+    for i in range(ret.shape[0]):
+        for j in range(ret.shape[1]):
+            ret[i][j] = w_dot(ret[i][j])
+    return ret
+
+im1 = np.asarray(Image.open(argv[1]).convert('RGB'))
+side_by_side.sbys_histogram([im1, uniform_hist(im1)], ['rgb']) 
+print(im1)
+
--- practica2/side_by_side.py
+++ practica2/side_by_side.py
@@ -45,15 +45,15 @@ def sbys_histogram(ims, mode, titles=None, argv=None):
         2*n, 4, width_ratios=[2, 1, 1, 1], height_ratios=[1 for _ in range(2*n)])
 
     for i in range(n):
-        plt.subplot(gs[i:i+2, 0]).imshow(imgs[i])
+        plt.subplot(gs[2*i:2*i+2, 0]).imshow(imgs[i])
     for i in range(n):
         dom, histogram_r, accum_r, histogram_g, accum_g, histogram_b, accum_b = histogram_rgb(ims[i])
-        plt.subplot(gs[2*i+1]).plot(range(L), histogram_r, 'r-')
-        plt.subplot(gs[2*i+2]).plot(range(L), histogram_g, 'g-')
-        plt.subplot(gs[2*i+3]).plot(range(L), histogram_b, 'b-')
-        plt.subplot(gs[2*i+4+1]).plot(range(L), accum_r, 'r--')
-        plt.subplot(gs[2*i+4+2]).plot(range(L), accum_g, 'g--')
-        plt.subplot(gs[2*i+4+3]).plot(range(L), accum_b, 'b--')
+        plt.subplot(gs[8*i+1]).plot(range(L), histogram_r, 'r-')
+        plt.subplot(gs[8*i+2]).plot(range(L), histogram_g, 'g-')
+        plt.subplot(gs[8*i+3]).plot(range(L), histogram_b, 'b-')
+        plt.subplot(gs[8*i+4+1]).plot(range(L), accum_r, 'r--')
+        plt.subplot(gs[8*i+4+2]).plot(range(L), accum_g, 'g--')
+        plt.subplot(gs[8*i+4+3]).plot(range(L), accum_b, 'b--')
     if titles is not None:
         for i in range(n):
             plt.subplot(gs[i]).set_title(titles[i])
