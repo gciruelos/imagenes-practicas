b0a4c902c680ddf62001ddb82f0a95b13ca65805
Gonzalo Ciruelos <gonzalo.ciruelos@gmail.com>
2017-11-22 12:44:15 -0300

    hough

---
+++ practica7/side_by_side.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+from PIL import Image
+import numpy as np
+
+
+def sbys(imgs, titles=None, argv=None, m=1):
+    whs = list(map(lambda x: x.size, imgs))
+    n = len(whs)
+    gs = gridspec.GridSpec(m, n // m)
+    plt.figure(figsize=(14, 7))
+
+    for i in range(n):
+        plt.subplot(gs[i]).imshow(imgs[i], cmap='gray') #, vmin=0, vmax=255)
+    if titles is not None:
+        for i in range(n):
+            plt.subplot(gs[i]).set_title(titles[i])
+    if argv is not None:
+        plt.savefig(argv, bbox_inches='tight')
+    else:
+        plt.show()
